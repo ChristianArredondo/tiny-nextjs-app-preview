2d612b6efac089547a0f8c7858fe44e899ab4442
Christian Arredondo <christianiarredondo@gmail.com>
2023-09-11 14:51:54 -0700

    Add db connection for vercel preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import mongoClientPromise from '@/services/mongodb';
 import styles from './page.module.css'
 
 export default async function Home() {
+  const mongoClient = await mongoClientPromise;
+  const db = mongoClient.db();
+  console.log('Connected to MongoDB');
+  console.log('DB name: ', db.databaseName);
   
   return (
     <main className={styles.main}>
