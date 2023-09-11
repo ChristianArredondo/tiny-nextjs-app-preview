dcaf4dea4eb9735822e14ad2aee55fb8b608bcc4
Christian Arredondo <christianiarredondo@gmail.com>
2023-09-11 13:57:45 -0700

    Remove mongodb connection at root page
    Temporarily remove to test vercel deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import mongoClientPromise from '@/services/mongodb';
 import styles from './page.module.css'
 
 export default async function Home() {
-  await mongoClientPromise;
-  console.log('Successfully connected to MongoDB');
   
   return (
     <main className={styles.main}>
