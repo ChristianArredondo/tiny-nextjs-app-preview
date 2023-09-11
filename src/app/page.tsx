44ccefe342153b0ed0026aa622df398433d10c1d
Christian Arredondo <christianiarredondo@gmail.com>
2023-09-11 15:01:45 -0700

    Render label from mongo atlas doc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,18 @@ import styles from './page.module.css'
 
 export default async function Home() {
   const mongoClient = await mongoClientPromise;
-  const db = mongoClient.db();
-  console.log('Connected to MongoDB');
-  console.log('DB name: ', db.databaseName);
+  const initDoc = await mongoClient
+    .db()
+    .collection('init')
+    .findOne();
   
   return (
     <main className={styles.main}>
       <div>
-        Hello. Tiny NextJS app is up and running :)
+        <p>
+          Hello. Tiny NextJS app is up and running :)
+        </p>
+        <p>{initDoc?.label}</p>
       </div>
     </main>
   );
