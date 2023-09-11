4b3b38503718bf0c40fd454ea74a0fe74a88b00b
Christian Arredondo <christianiarredondo@gmail.com>
2023-09-11 14:08:02 -0700

    Update root page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   return (
     <main className={styles.main}>
       <div>
-        Hello. Your tiny NextJS app is up and running :)
+        Hello. Tiny NextJS app is up and running :)
       </div>
     </main>
   );
